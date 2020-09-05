33ec02cb17d7d2e8aad262802aa8370648443aa3
Oreo59 <rcharnoky@gmail.com>
2020-09-05 13:09:09 -0400

    Adding React website

---
+++ src/components/aboutme.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <br></br>
+        <h2 class="about-me-header">I am an ambitious and motivated student</h2>
+        <p class="about-me-header">I am in my fifth year of a dual degree program at The University of Georgia.  
+        Currently I am studying aborad at Karlsruhe Institute of Technology in Karlsruhe, Germany. 
+        By the time I graduate, I will have completed two semesters of co-op work experience with two different companies.  
+        In addition, I will have completed two Summer internships as well as have participated in numerous clubs in college.
+        I hope to pursue my passion of helping others through my work in computer science.  </p>
+      </div>
+    )
+  }
+}
+
+export default About;
---
+++ src/components/landingpage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+
+class Landing extends Component {
+  render() {
+    return (
+      <div style={{ width: '100%', margin: 'auto' }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://www.c7creative.com/wp-content/uploads/2015/05/Lee_C7_Avatar-01.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+
+              <p>Flutter | HTML/CSS | JavaScript | React | Amazon AWS | Java | C++</p>
+
+              <div className="social-links">
+
+
+                {/* Github */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+                {/* LinkedIn */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/* Youtube */}
+                <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-youtube-square" aria-hidden="true" />
+                </a>
+
+              </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Landing;
