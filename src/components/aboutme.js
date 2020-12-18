82b7d30c19a03ba9f8fd517e5c8f20ff1e0d9ad3
Oreo59 <rcharnoky@gmail.com>
2020-12-18 12:57:38 -0500

    Changes to aboutme page 2

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class About extends Component {
   render() {
     return (
-      <div>
-        <br></br>
-        <h2 class="about-me-header">I am an ambitious and motivated student</h2>
-        <p class="about-me-header">I am in my fifth year of a dual degree program at The University of Georgia.  
-        Currently I am studying aborad at Karlsruhe Institute of Technology in Karlsruhe, Germany. 
-        By the time I graduate, I will have completed two semesters of co-op work experience with two different companies.  
-        In addition, I will have completed two Summer internships as well as have participated in numerous clubs in college.
-        I hope to pursue my passion of helping others through my work in computer science.  </p>
+      <div className="aboutme-page">
+        <Grid className="aboutme-background">
+          <Cell col={9}>
+            <br></br>
+            <h2 className="about-me-header">Ambitious & Motivated</h2>
+            <h4 className="about-me-header" style={{padding: '40px'}}>
+              Currently finishing my duel degree in Computer Systems Engineering and German.  
+              I love a fast paced and cutting edge environment, and that's where I found programming.  
+              With each new project or internship, I immerse myself in a new technology and figure it out.  
+              Besides programming, I study German and love learning about new cultures and meeting new people.  
+              After work you can find me playing sports or working out, or playing with my three dogs.  
+            </h4>
+          </Cell>
+          <Cell col={3} >
+            <div>
+              <img
+                alt="myimage"
+                src={"https://scontent-atl3-2.cdninstagram.com/v/t51.2885-19/s320x320/90023994_205237570725091_3920293921262927872_n.jpg?_nc_ht=scontent-atl3-2.cdninstagram.com&_nc_ohc=px9I9W-8HqUAX9htIRQ&tp=1&oh=ab0aac951f6e41660e2f18bb5b154883&oe=6005D897"}
+                style={{ height: "300px", borderRadius: '200px', padding: '20px' }} />
+              <h3 className='about-me-header'>
+                Ryan Charnoky
+              </h3>
+            </div>
+          </Cell>
+        </Grid>
+        <hr style={{ borderTop: '3px solid #e22947' }} />
+        <Grid style={{padding: '0px'}}>
+        <img
+              src="https://image.freepik.com/free-vector/abstract-dotted-banner-background_1035-18160.jpg"
+              alt="avatar"
+              style={{height: '190px', width: '100%'}}
+               />
+        </Grid>
       </div>
     )
   }
